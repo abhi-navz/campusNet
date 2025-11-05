3de890b6b93d6c5317b6769b425255cd07153ec5
abhi-navz <abhinavg01441@gmail.com>
2025-11-05 18:50:54 +0530

    Set up the structure of backend Controllers, Routes, usermodel and connected database using mongoDB

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page working now");
+});
+app.use("/auth", authRoutes);
+
+export default app;

a1db3922bd9995184f00835770975c65e651caf1
shivashishyadav <ay2084563@gmail.com>
2025-11-29 04:54:21 +0530

    Fix import paths for backend deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
-
-import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
